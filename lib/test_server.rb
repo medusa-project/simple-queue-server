72e1e28d58bc44333fd228bc413fa994317cbe0a
Howard Ding <hding2@illinois.edu>
2014-10-29 15:54:27 -0500

    Add example test server.

---
+++ lib/test_server.rb
@@ -0,0 +1,13 @@
+#This is a really simple example of how you can implement a server. It has a single action, 'square', which requires
+#a single numeric parameter 'number' and returns the square of that number.
+
+require_relative 'simple_amqp_server'
+
+class TestServer < SimpleAmqpServer
+
+  def handle_square_request(interaction)
+    number = interaction.request_parameter('number')
+    interaction.succeed(square: number * number)
+  end
+
+end
