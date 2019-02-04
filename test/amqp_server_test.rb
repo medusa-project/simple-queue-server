6a329609d65787fa6ce99425b5ef05efdc8e058f
Howard Ding <hding2@illinois.edu>
2019-02-04 13:08:50 -0600

    Add trivial test to start testing setup and add travis

---
+++ test/amqp_server_test.rb
@@ -0,0 +1,9 @@
+require_relative 'test_helper'
+
+class AmqpServerTest < Minitest::Test
+
+  def test_trivial
+    assert true
+  end
+
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+require_relative '../lib/simple_amqp_server'
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'minitest/pride'
